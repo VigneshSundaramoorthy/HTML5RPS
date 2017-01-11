0019b4bc91360e0f52f12e44939fe7c8c7f9087e
Vignesh Sundar <vickysundar@live.in>
2017-01-11 02:40:23 -0800

    Web worker and threading

---
+++ HTML5WebApp1/HTML5WebApp1/WebWorker/RandomWords.js
@@ -0,0 +1,18 @@
+﻿var words = new Array("apple", "chair", "person", "door", "tesla", "Harry", "Stark");
+var randomIndex = 0;
+function PickWord() {
+    for (; ;) {
+        var newIndex = Math.floor(Math.random() * (words.length));
+        if (newIndex != randomIndex) {
+            randomIndex = newIndex;
+            break;
+        }
+    }
+
+    var word = words[randomIndex];
+
+    postMessage(word);
+    setTimeout("PickWord()", 1500);
+}
+
+PickWord();
